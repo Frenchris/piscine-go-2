e19d2e6ee76b65a296f05d838e60a5171907b29a
OGordoo <marcosafonso8@hotmail.com>
2019-09-24 16:28:08 +0100

    BasicAtoi2

---
+++ basicatoi2.go
@@ -0,0 +1,39 @@
+package piscine
+
+func BasicAtoi2(s string) int{
+	stri := []rune(s)
+
+	res:=0
+
+	for i := 0; i < StrLength(s) ; i++  {
+
+		if '0' <= stri[i] && stri[i] <= '9'{
+			exp:= StrLength(s) - 1 - i
+			if stri[i] == '0'{
+				res += 0 * Power(10,exp) 
+			} else if stri[i] == '1'{
+				res += 1 * Power(10,exp) 
+			} else if stri[i] == '2'{
+				res += 2 * Power(10,exp) 
+			} else if stri[i] == '3'{
+				res += 3 * Power(10,exp) 
+			} else if stri[i] == '4'{
+				res += 4 * Power(10,exp) 
+			} else if stri[i] == '5'{
+				res += 5 * Power(10,exp) 
+			} else if stri[i] == '6'{
+				res += 6 * Power(10,exp) 
+			} else if stri[i] == '7'{
+				res += 7 * Power(10,exp) 
+			} else if stri[i] == '8'{
+				res += 8 * Power(10,exp) 
+			} else if stri[i] == '9'{
+				res += 9 * Power(10,exp) 
+			}
+		} else {
+			return 0
+		}
+	} 
+	
+	return res
+}
