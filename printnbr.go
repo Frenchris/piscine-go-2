c0f658c1708ec3b7da8d56fa11ac5ba862309099
OGordoo <marcosafonso8@hotmail.com>
2019-09-24 11:42:37 +0100

    PrintNbr

--- printnbr.go
+++ printnbr.go
@@ -6,17 +6,17 @@ import (
 
 func PrintNbr(n int){
 
+	t:= 1
 	if n < 0 {
 		z01.PrintRune('-')
-		
-		n = n*-1
+		t = -1
 	}
 	if n != 0 {
-		q := (n / 10)
+		q := (n / 10) * t
 		if q != 0{
 			PrintNbr(q)
 		}
-		d := (n % 10) +'0'
+		d := ((n % 10) * t) +'0'
 		z01.PrintRune(rune(d))
 	} else {
 		z01.PrintRune('0')
