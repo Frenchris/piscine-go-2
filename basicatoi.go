8c0d0a8294bda12633a2706f6dbc76c018e12e5b
OGordoo <marcosafonso8@hotmail.com>
2019-09-24 16:11:51 +0100

    basicatoi

---
+++ basicatoi.go
@@ -0,0 +1,47 @@
+package piscine
+
+func BasicAtoi(s string) int{
+	stri := []rune(s)
+
+	res:=0
+
+	for i := 0; i < StrLength(s) ; i++  {
+			exp:= StrLength(s) - 1 - i
+			if stri[i] == '0'{
+				res += 0 * Power(10,exp) 
+			} else if stri[i] == '1'{
+				res += 1 * Power(10,exp) 
+			} else if stri[i] == '2'{
+				res += 2 * Power(10,exp) 
+			} else if stri[i] == '3'{
+				res += 3 * Power(10,exp) 
+			} else if stri[i] == '4'{
+				res += 4 * Power(10,exp) 
+			} else if stri[i] == '5'{
+				res += 5 * Power(10,exp) 
+			} else if stri[i] == '6'{
+				res += 6 * Power(10,exp) 
+			} else if stri[i] == '7'{
+				res += 7 * Power(10,exp) 
+			} else if stri[i] == '8'{
+				res += 8 * Power(10,exp) 
+			} else if stri[i] == '9'{
+				res += 9 * Power(10,exp) 
+			}
+		} 
+	
+	return res
+}
+
+func Power(n int, m int) int{
+	helper := n
+	if m == 0{
+		n = 1
+	} else {
+		for i := 1; i < m; i++ {
+			n *= helper
+		}
+	}
+	return n
+}
+
