814214dcf803319e01011e50972281ac2c7b103e
OGordoo <marcosafonso8@hotmail.com>
2019-09-26 16:40:45 +0100

    Compare

--- toupper.go
+++ toupper.go
@@ -1,20 +1,14 @@
 package piscine
 
-import "fmt"
-
 func ToUpper(s string) string {
 
 	str := []rune(s)
-
-	fmt.Println(str)
 
 	for i := 0; i < StrLen(s); i++ {
 		if str[i] >= 97 && str[i] <= 122 {
 			str[i] -= 32
 		}
 	}
-
-	fmt.Println(str)
 
 	return string(str)
 
