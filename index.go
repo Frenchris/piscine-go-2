144f994a49a4fc1874b647a7d03fa58c1ff93ee4
OGordoo <marcosafonso8@hotmail.com>
2019-09-26 15:54:55 +0100

    Index

--- index.go
+++ index.go
@@ -6,26 +6,55 @@ func Index(s string, toFind string) int {
 	toFindArr := []rune(toFind)
 	count := 0
 	i := 0
-	count2 := 0
+	pos := 0
+	firstLetter := 0
 
 	for j := 0; j < StrLen(s); j++ {
 		if sArr[j] == toFindArr[i] {
 			count++
+			if firstLetter == 0 {
+				pos = j
+				firstLetter++
+			}
 			i++
 
 			if count == StrLen(toFind) {
 				i = 0
-				count = 0
-				count2++
-
+				return pos
 			}
 		}
-
 	}
 
-	if count2 == 0 {
-		count2--
+	if count == 0 {
+		pos = -1
 	}
 
-	return count2
+	return pos
+
+	// sArr := []rune(s)
+	// toFindArr := []rune(toFind)
+	// count := 0
+	// i := 0
+	// count2 := 0
+
+	// for j := 0; j < StrLen(s); j++ {
+	// 	if sArr[j] == toFindArr[i] {
+	// 		count++
+	// 		i++
+
+	// 		if count == StrLen(toFind) {
+	// 			i = 0
+	// 			count = 0
+	// 			count2++
+
+	// 		}
+	// 	}
+
+	// }
+
+	// if count2 == 0 {
+	// 	count2--
+	// }
+
+	// return count2
 }
