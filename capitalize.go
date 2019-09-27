0067de3873012188c0fd60c1bb6698e600f84e5d
OGordoo <marcosafonso8@hotmail.com>
2019-09-27 10:36:47 +0100

    Cap

--- capitalize.go
+++ capitalize.go
@@ -2,19 +2,34 @@ package piscine
 
 func Capitalize(s string) string {
 	str := []rune(s)
-	if str[0] >= 97 && str[0] <= 122 {
+	if str[0] >= 'a' && str[0] <= 'z' {
 		str[0] -= 32
 	}
-	for i := 0; i < StrLen(s); i++ {
-		if !((str[i] >= '0' && str[i] <= '9') ||
-			(str[i] >= 'a' && str[i] <= 'z') ||
-			(str[i] >= 'A' && str[i] <= 'Z')) {
+
+	for i := 1; i < StrLen(s); i++ {
+		if verCharacter(str, i) {
+			if str[i] >= 'A' && str[i] <= 'Z' {
+				if verCharacter(str, i-1) {
+					str[i] += 32
+				}
+			}
+		} else {
 			if i != StrLen(s)-1 {
-				if str[i+1] >= 'a' && str[i+1] <= 'z' {
+				if str[i+1] >= 97 && str[i+1] <= 122 {
 					str[i+1] -= 32
 				}
 			}
 		}
 	}
 	return string(str)
+}
+
+func verCharacter(str []rune, i int) bool {
+	if (str[i] >= '0' && str[i] <= '9') ||
+		(str[i] >= 'a' && str[i] <= 'z') ||
+		(str[i] >= 'A' && str[i] <= 'Z') {
+		return true
+	}
+
+	return false
 }
