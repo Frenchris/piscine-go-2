d6f19fc6f36db083363636d24a2e3cfbf9306784
OGordoo <marcosafonso8@hotmail.com>
2019-09-27 16:40:53 +0100

    print args

--- printparams/printparams.go
+++ printparams/printparams.go
@@ -1,15 +1,18 @@
-package printparams
+package main
 
 import (
 	"os"
 
 	piscine ".."
+
+	"github.com/01-edu/z01"
 )
 
 func main() {
 
 	for i := 1; i < piscine.ArrayStrLength(os.Args); i++ {
 		piscine.PrintStr(os.Args[i])
+		z01.PrintRune('\n')
 	}
 
 }
