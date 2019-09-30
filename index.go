2f3c3ae0cd3bc8f358b0e8a80281bfae8f32be54
OGordoo <marcosafonso8@hotmail.com>
2019-09-30 17:39:54 +0100

    count instances func

--- index.go
+++ index.go
@@ -40,30 +40,33 @@ func Index(s string, toFind string) int {
 		return pos
 	}
 
-	// sArr := []rune(s)
-	// toFindArr := []rune(toFind)
-	// count := 0
-	// i := 0
-	// count2 := 0
+}
 
-	// for j := 0; j < StrLen(s); j++ {
-	// 	if sArr[j] == toFindArr[i] {
-	// 		count++
-	// 		i++
+func CountInstances(s string, toFind string) int {
+	sArr := []rune(s)
+	toFindArr := []rune(toFind)
+	count := 0
+	i := 0
+	count2 := 0
 
-	// 		if count == StrLen(toFind) {
-	// 			i = 0
-	// 			count = 0
-	// 			count2++
+	for j := 0; j < StrLen(s); j++ {
+		if sArr[j] == toFindArr[i] {
+			count++
+			i++
 
-	// 		}
-	// 	}
+			if count == StrLen(toFind) {
+				i = 0
+				count = 0
+				count2++
 
-	// }
+			}
+		}
 
-	// if count2 == 0 {
-	// 	count2--
-	// }
+	}
 
-	// return count2
+	if count2 == 0 {
+		count2--
+	}
+
+	return count2
 }
