d36fca861c47c15b4ac78e9c871e79660737a337
OGordoo <marcosafonso8@hotmail.com>
2019-09-30 17:49:15 +0100

    Split

--- split.go
+++ split.go
@@ -17,27 +17,3 @@ func Split(str, charset string) []string {
 
 	return result
 }
-
-// result := make([]string, count+1)
-// stri := ""
-// j := 0
-// for i := 0; i < StrLen(string(sArray)); i++ {
-// 	if string(sArray[i]) == charset {
-
-// 		stri = string(sArray[0:i])
-// 		if string(sArray[i+1]) == " " {
-// 			sArray = sArray[i+2 : StrLen(string(sArray))]
-
-// 		} else {
-// 			sArray = sArray[i+1 : StrLen(string(sArray))]
-// 		}
-// 		result[j] = stri
-// 		i = -1
-// 		j++
-// 		//fmt.Println(string(sArray))
-// 	}
-// 	if j == count {
-// 		result[j] = string(sArray[0:StrLen(string(sArray))])
-// 	}
-// }
-// return result
