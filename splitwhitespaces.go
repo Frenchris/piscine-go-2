a3ce4af606a023449ad4d6b7d77f0dbac70ec6cf
OGordoo <marcosafonso8@hotmail.com>
2019-09-30 12:37:16 +0100

    SplitWhiteSpaces

--- splitwhitespaces.go
+++ splitwhitespaces.go
@@ -1,13 +1,10 @@
 package piscine
 
-import "fmt"
-
 func SplitWhiteSpaces(str string) []string {
 	count := 0
 	sArray := []rune(str)
 	for i := 0; i < StrLen(str); i++ {
 		if i != StrLen(str)-1 {
-
 			if sArray[i] == '\n' || sArray[i] == '\t' || sArray[i] == ' ' {
 				if !(sArray[i+1] == '\n' || sArray[i+1] == '\t' || sArray[i+1] == ' ') {
 					count++
@@ -21,9 +18,6 @@ func SplitWhiteSpaces(str string) []string {
 	for i := 0; i < StrLen(string(sArray)); i++ {
 		if sArray[i] == '\n' || sArray[i] == '\t' || sArray[i] == ' ' || sArray[i] == '\v' {
 
-			fmt.Println(sArray)
-			fmt.Println(string(sArray))
-			fmt.Println()
 			stri = string(sArray[0:i])
 			if string(sArray[i+1]) == " " {
 				sArray = sArray[i+2 : StrLen(string(sArray))]
@@ -31,15 +25,11 @@ func SplitWhiteSpaces(str string) []string {
 			} else {
 				sArray = sArray[i+1 : StrLen(string(sArray))]
 			}
-			fmt.Println(sArray)
-			fmt.Println(string(sArray))
 			result[j] = stri
 			i = -1
 			j++
 			//fmt.Println(string(sArray))
 		}
-		fmt.Println(count, j)
-		fmt.Println(string(sArray[0:StrLen(string(sArray))]))
 		if j == count {
 			result[j] = string(sArray[0:StrLen(string(sArray))])
 		}
