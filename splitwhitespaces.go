01eb89326aa144708cad171314e111571cd6f2a3
OGordoo <marcosafonso8@hotmail.com>
2019-09-30 12:20:17 +0100

    SplitWhiteSpaces

--- splitwhitespaces.go
+++ splitwhitespaces.go
@@ -1,5 +1,7 @@
 package piscine
 
+import "fmt"
+
 func SplitWhiteSpaces(str string) []string {
 	count := 0
 	sArray := []rune(str)
@@ -13,17 +15,30 @@ func SplitWhiteSpaces(str string) []string {
 	j := 0
 	for i := 0; i < StrLen(string(sArray)); i++ {
 		if sArray[i] == '\n' || sArray[i] == '\t' || sArray[i] == ' ' || sArray[i] == '\v' {
+
+			fmt.Println(sArray)
+			fmt.Println(string(sArray))
+			fmt.Println()
 			stri = string(sArray[0:i])
-			sArray = sArray[i+1 : StrLen(string(sArray))]
+			if string(sArray[i+1]) == " " {
+				sArray = sArray[i+2 : StrLen(string(sArray))]
+			} else {
+				sArray = sArray[i+1 : StrLen(string(sArray))]
+			}
+			fmt.Println(sArray)
+			fmt.Println(string(sArray))
 			result[j] = stri
-			i = 0
+			i = -1
+
 			j++
 			//fmt.Println(string(sArray))
 		}
-		if j == count {
+		if j == count-1 {
 			result[j] = string(sArray[0:StrLen(string(sArray))])
 		}
 	}
 
+	fmt.Println([]rune(result[0]))
+	fmt.Println()
 	return result
 }
