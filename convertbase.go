3a8ff893269c49807320c260959c58b8ac975661
OGordoo <marcosafonso8@hotmail.com>
2019-09-30 16:18:13 +0100

    ConvertBase

--- convertbase.go
+++ convertbase.go
@@ -2,18 +2,8 @@ package piscine
 
 func ConvertBase(nbr, baseFrom, baseTo string) string {
 
-	negative := false
-	if baseFrom == baseTo {
-		return nbr
-	} else if baseTo == "0123456789" {
-		return Itoa(AtoiBase(nbr, baseFrom))
-	} else if baseFrom == "0123456789" {
-		SeeNumber(Atoi(nbr), baseTo, negative)
-	} else {
-		helper := string(AtoiBase(nbr, "0123456789"))
-		SeeNumber(Atoi(helper), baseTo, negative)
-	}
-	return ""
+	helper := AtoiBase(nbr, baseFrom)
+	return ConvertFromDecimal(helper, baseTo)
 }
 
 func ConvertFromDecimal(nbr int, base string) string {
