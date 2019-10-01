645c5d6828db6c8c9c7ec5558bb42c9eadaeeffb
OGordoo <marcosafonso8@hotmail.com>
2019-10-01 14:34:58 +0100

    Cat

--- cat/cat.go
+++ cat/cat.go
@@ -18,6 +18,7 @@ func main() {
 			if os.Args[i] == "quest8.txt" {
 				if i+1 == nbrArguments {
 					piscine.PrintFile("quest8.txt")
+					break
 				} else {
 					piscine.PrintFile("quest8.txt")
 					z01.PrintRune('\n')
@@ -25,6 +26,7 @@ func main() {
 			} else if os.Args[i] == "quest8T.txt" {
 				if i+1 == nbrArguments {
 					piscine.PrintFile("quest8T.txt")
+					break
 				} else {
 					piscine.PrintFile("quest8T.txt")
 					z01.PrintRune('\n')
