4eb7831825be19f2a8209dbf9c79c13aef8dbca2
OGordoo <marcosafonso8@hotmail.com>
2019-10-01 13:49:07 +0100

    DisplayFile

---
+++ displayfile/displayfile.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"os"
+
+	piscine ".."
+	"github.com/01-edu/z01"
+)
+
+func main() {
+
+	lengthArgs := piscine.ArrayStrLength(os.Args)
+	if lengthArgs >= 3 {
+		piscine.PrintStr("Too many arguments")
+		z01.PrintRune('\n')
+	} else if lengthArgs == 1 {
+		piscine.PrintStr("File name missing")
+		z01.PrintRune('\n')
+	} else if os.Args[1] == "quest8.txt" {
+
+		fileRe, err := os.Open("quest8.txt")
+
+		stat, err := fileRe.Stat()
+
+		if err != nil {
+			piscine.PrintStr(err.Error())
+			return
+		}
+		arr := make([]byte, stat.Size())
+
+		fileRe.Read(arr)
+
+		piscine.PrintStr(string(arr))
+		z01.PrintRune('\n')
+
+	}
+
+}
