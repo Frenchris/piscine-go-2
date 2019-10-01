b65ec7d00e673d71f97a119abb611028c24d48e2
OGordoo <marcosafonso8@hotmail.com>
2019-10-01 16:05:47 +0100

    IsSorted

--- issorted.go
+++ issorted.go
@@ -1,15 +1,25 @@
 package piscine
 
 func IsSorted(f func(a, b int) int, tab []int) bool {
+	ascOrder := 1
+	decOrder := 1
+
 	for i := 0; i < ArrayIntLength(tab)-1; i++ {
-		if f(tab[i], tab[i+1]) > 0 {
-			return false
+		if f(tab[i], tab[i+1]) >= 0 {
+			ascOrder++
+		} else if f(tab[i], tab[i+1]) <= 0 {
+			decOrder++
 		}
 	}
-	return true
+
+	if ascOrder == ArrayIntLength(tab) || decOrder == ArrayIntLength(tab) {
+		return true
+	}
+	return false
+
 }
 
-func f(a, b int) int {
+func F(a, b int) int {
 	if a > b {
 		return 1
 	} else if a == b {
