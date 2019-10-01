6136355a3100f9df235e7cb80ff6de0261868f0b
OGordoo <marcosafonso8@hotmail.com>
2019-10-01 17:23:02 +0100

    IsNumber

--- isnumeric.go
+++ isnumeric.go
@@ -9,3 +9,17 @@ func IsNumeric(str string) bool {
 	}
 	return true
 }
+
+func IsNumber(str string) bool {
+
+	if str[0] == '-' {
+		str = str[1:StrLen(str)]
+	}
+
+	for i := 0; i < StrLen(str); i++ {
+		if !(str[i] >= '0' && str[i] <= '9') {
+			return false
+		}
+	}
+	return true
+}
