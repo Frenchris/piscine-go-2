e6de0a5b5cfbfc3377b6188ac97fca687e3a736d
OGordoo <marcosafonso8@hotmail.com>
2019-10-01 15:39:48 +0100

    ForEach

--- foreach.go
+++ foreach.go
@@ -1,12 +1,8 @@
 package piscine
 
-import (
-	"github.com/01-edu/z01"
-)
-
 func ForEach(f func(int), arr []int) {
 	for i := 0; i < ArrayIntLength(arr); i++ {
 		f(arr[i])
 	}
-	z01.PrintRune('\n')
+
 }
