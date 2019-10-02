d2a3091197005dca0d00b2b627455a48aad3ed09
OGordoo <marcosafonso8@hotmail.com>
2019-10-02 12:45:35 +0100

    Join

--- join.go
+++ join.go
@@ -4,7 +4,7 @@ func Join(strs []string, sep string) string {
 
 	result := ""
 	for i := 0; i < ArrayStrLength(strs)-1; i++ {
-		result += strs[i] + ":"
+		result += strs[i] + sep
 	}
 	result += strs[ArrayStrLength(strs)-1]
 
