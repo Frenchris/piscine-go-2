e853365c347dc12fc3d1ea8f6d5633b14c537895
OGordoo <marcosafonso8@hotmail.com>
2019-10-03 10:18:56 +0100

    ListLast

---
+++ listlast.go
@@ -0,0 +1,12 @@
+package piscine
+
+func ListLast(l *List) interface{} {
+
+	for l.Head != nil {
+		if l.Head.Next == nil {
+			return l.Head.Data
+		}
+		l.Head = l.Head.Next
+	}
+	return l.Head
+}
