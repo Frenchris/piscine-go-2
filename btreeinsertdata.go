605fcff5b24fe3330c785ed38aa82bbc46e4b326
OGordoo <marcosafonso8@hotmail.com>
2019-10-04 14:08:22 +0100

    BTreeInsertData

--- btreeinsertdata.go
+++ btreeinsertdata.go
@@ -7,5 +7,14 @@ type TreeNode struct {
 
 func BTreeInsertData(root *TreeNode, data string) *TreeNode {
 
+	if root == nil {
+		return &TreeNode{Data: data}
+	}
+	if data < root.Data {
+		root.Left = BTreeInsertData(root.Left, data)
+	} else {
+		root.Right = BTreeInsertData(root.Right, data)
+	}
 	return root
+
 }
